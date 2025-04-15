194c5cb0fd3549a2dcd1e7efcd8da5bf0feb0b25
leoaoluis <118204341+leoaoluis@users.noreply.github.com>
2025-04-15 11:51:39 +0100

    Update Aquarium.js

--- static/js/objects/aquarium/Aquarium.js
+++ static/js/objects/aquarium/Aquarium.js
@@ -8,18 +8,16 @@ export class Aquarium {
     this.loadAquarium();
   }
 
-  // Função para pegar o caminho absoluto, mas vai utilizar o caminho relativo para os arquivos
   getAbsolutePath(relativePath) {
     return new URL(relativePath, window.location.href).href;
   }
 
-  // Função para carregar o aquário
   loadAquarium() {
     const basePath = '/static/assets/models/aquarium/';
     const modelName = 'aquario';
 
-    const fullMtlPath = `${basePath}${modelName}.mtl`;
-    const fullObjPath = `${basePath}${modelName}.obj`;
+    const fullMtlPath = this.getAbsolutePath(`${basePath}${modelName}.mtl`);
+    const fullObjPath = this.getAbsolutePath(`${basePath}${modelName}.obj`);
 
     console.log(`Carregando aquário de: ${fullMtlPath}`);
 
@@ -28,7 +26,6 @@ export class Aquarium {
 
       const objLoader = new OBJLoader2();
 
-      // Se o método setMaterials estiver disponível, configuramos os materiais
       if (typeof objLoader.setMaterials === 'function') {
         objLoader.setMaterials(materials);
       } else {
@@ -36,16 +33,13 @@ export class Aquarium {
         objLoader.addMaterials(materials.materials, true);
       }
 
-      // Carregando o modelo OBJ do aquário
       objLoader.load(fullObjPath, (event) => {
         const root = event.detail?.loaderRootNode || event;
 
-        // Definindo propriedades do modelo (escala, rotação, posição)
         root.rotation.set(9.6, 0, 0);
         root.scale.set(0.5, 0.5, 0.5);
         root.position.set(0, 0, 0);
 
-        // Configurando sombras e materiais para o modelo
         root.traverse((child) => {
           if (child.isMesh) {
             child.castShadow = true;
@@ -56,7 +50,6 @@ export class Aquarium {
           }
         });
 
-        // Adicionando o modelo do aquário à cena
         this.mesh.add(root);
         this.addDirtOverlay();
         console.log('Aquário carregado e posicionado corretamente!');
@@ -68,7 +61,6 @@ export class Aquarium {
     });
   }
 
-  // Função para adicionar a camada de sujeira no aquário
   addDirtOverlay() {
     const textureLoader = new THREE.TextureLoader();
     const dirtTexture = textureLoader.load('/static/assets/models/textures/dirt_overlay.png');
@@ -86,6 +78,6 @@ export class Aquarium {
     dirtMesh.position.set(0, 1.5, -2.45); // Frente do aquário
 
     this.mesh.add(dirtMesh);
-    this.dirtMesh = dirtMesh; // Guardar referência
+    this.dirtMesh = dirtMesh; // guardar referência
   }
 }
